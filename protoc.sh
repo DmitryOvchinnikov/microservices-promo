2c00c188d5c5c7f4c0f66bf5a5382bf968be2da7
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2023-10-15 16:36:43 +0300

    proto update

--- protoc.sh
+++ protoc.sh
@@ -2,6 +2,7 @@
 
 USER_NAME="Dmitry Ovchinnikov"
 EMAIL="dmitry.v.ovchinnikov@gmail.com"
+RELEASE_VERSION="0.0.1"
 
 git config user.name "$USER_NAME"
 git config user.email "$EMAIL"
@@ -14,3 +15,11 @@ protoc -I shipping/ shipping/shipping.proto --go_out=./pb;
 cd ./pb
 go mod init github.com/dmitryovchinnikov/microservices-proto/pb
 go mod tidy
+
+cd ..
+
+git add . && git commit -am "proto update" || true
+git push origin HEAD:main
+#git tag -fa pb/${SERVICE_NAME}/${RELEASE_VERSION} -m "pb/${SERVICE_NAME}/${RELEASE_VERSION}"
+#git push origin refs/tags/pb/${SERVICE_NAME}/${RELEASE_VERSION}
+
