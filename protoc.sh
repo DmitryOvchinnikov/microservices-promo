3902d6c49b9a5bd3db85dc60f1990ddea0bb99c2
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2023-10-15 16:39:34 +0300

    proto update

--- protoc.sh
+++ protoc.sh
@@ -8,9 +8,9 @@ git config user.name "$USER_NAME"
 git config user.email "$EMAIL"
 git fetch --all && git checkout main
 
-protoc -I order/ order/order.proto --go_out=./pb;
-protoc -I payment/ payment/payment.proto --go_out=./pb;
-protoc -I shipping/ shipping/shipping.proto --go_out=./pb;
+protoc -I order/ order/order.proto --go_out=./pb --go-grpc_out=./pb
+protoc -I payment/ payment/payment.proto --go_out=./pb --go-grpc_out=./pb
+protoc -I shipping/ shipping/shipping.proto --go_out=./pb --go-grpc_out=./pb
 
 cd ./pb
 go mod init github.com/dmitryovchinnikov/microservices-proto/pb
